a7652918af8d640b193706597a95f3280cfa2ab4
wenjun <wjxu@thoughtworks.com>
2021-01-14 23:32:47 +0800

    feat: add antd & iconfont

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
+import {Button} from "antd";
+
+import "antd/dist/antd.css";
+// import "./assets/fonts/iconfont.css";
+
 import './App.css';
+import {EXPENDITURE_ICON_LIST, INCOME_ICON_LIST} from "./constants";
 
 function App() {
   return (
     <div className="app">
-     Hello World
+      <Button>Hello World</Button>
+      <div>
+        {
+          INCOME_ICON_LIST.map(item =>
+
+            <svg className={"icon"}>
+              <use xlinkHref={`#${item.icon}`}/>
+            </svg>
+          )
+        }
+      </div>
+      <div>
+
+        {
+          EXPENDITURE_ICON_LIST.map(item =>
+
+            <svg className={"icon"}>
+              <use xlinkHref={`#${item.icon}`}/>
+            </svg>
+          )
+        }
+      </div>
+
+
     </div>
   );
 }
